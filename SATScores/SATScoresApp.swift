70d3fe829e0bc911fcc9444453f16cbf3516e08f
JD Paul <jdpaul123@gmail.com>
2024-01-24 21:47:55 -0800

    Initial Commit

---
+++ SATScores/SATScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  SATScoresApp.swift
+//  SATScores
+//
+//  Created by Jonathan Paul on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SATScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
